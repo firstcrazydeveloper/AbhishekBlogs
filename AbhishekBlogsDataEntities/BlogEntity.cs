a18764f572330312af7e0634d78a049cfe2cccfd
First Crazy Developer <support@firstcrazydeveloper.com>
2016-03-25 15:59:29 +0530

    Update MVC Reference

--- AbhishekBlogsDataEntities/BlogEntity.cs
+++ AbhishekBlogsDataEntities/BlogEntity.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
 
 namespace AbhishekBlogsDataEntities
 {
